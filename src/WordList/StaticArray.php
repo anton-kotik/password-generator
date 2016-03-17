5a693a159c84d1ae9a683fc7cce0da5399b9a42f
27cm <avti1308@yandex.ru>
2016-03-17 18:31:02 +0300

    Create StaticArray

---
+++ src/WordList/StaticArray.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace TwentySeven\Password\WordList;
+
+use TwentySeven\Password\WordListInterface;
+
+/**
+ * Word list stored in static array.
+ */
+abstract class StaticArray implements WordListInterface
+{
+    /**
+     * @var array Array of words.
+     */
+    protected static $words = [ 'correct', 'horse', 'battery', 'staple' ];
+
+    /**
+     * {@inheritdoc}
+     */
+    public function get($random)
+    {
+        $length = count(static::$words);
+        $position = (int) ($random * ($length - 1));
+        return static::$words[$position];
+    }
+}
